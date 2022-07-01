2a25f981c01f3073b9dbf967c0f5febcccd567ef
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-01 20:33:49 -0300

    chore: connect MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const connectToDatabase = require('./src/database/database');
 
 const port = 3000;
 
@@ -7,6 +8,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+connectToDatabase();
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
