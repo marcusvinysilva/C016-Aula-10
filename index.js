42ba3df071a460e1d6e38c8b8ee59891d57a5a4e
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-04 21:01:28 -0300

    feat: add swagger route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const connectToDatabase = require('./src/database/database');
 const paletasRoute = require('./src/routes/paletas.route');
+const swaggerRoute = require('./src/routes/swagger.route');
 
 const port = 3000;
 
@@ -13,6 +14,7 @@ app.use(express.json());
 connectToDatabase();
 
 app.use('/paletas', paletasRoute);
+app.use('/api', swaggerRoute);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
